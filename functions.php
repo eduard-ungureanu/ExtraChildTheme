7f68930cfe9d3939f30e7f3b0ed942c3a34f0102
Eduard Ungureanu <u.eduard.email@gmail.com>
2024-11-19 14:13:20 +0200

    Update functions.php
    Update the loading of .mo files due to changes in WordPress 6.7

--- functions.php
+++ functions.php
@@ -10,8 +10,9 @@ add_action( 'wp_enqueue_scripts', 'extra_enqueue_styles' );
 #Load the translations from the child theme folder
 ================================================*/
 function wpcninja_translation() {
-	load_child_theme_textdomain( 'extra', get_stylesheet_directory() . '/lang/theme/' );
-	load_child_theme_textdomain( 'et_builder', get_stylesheet_directory() . '/lang/builder/' );
+	
+	load_textdomain( 'extra', get_stylesheet_directory() . '/lang/theme/extra-ro_RO.mo' );
+	load_textdomain( 'et_builder', get_stylesheet_directory() . '/lang/builder/et_builder-ro_RO.mo' );
 
 }
 add_action( 'after_setup_theme', 'wpcninja_translation' );
